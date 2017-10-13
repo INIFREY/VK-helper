f4f12da882cff3b793e66664d5fef5bed858fb74
Valera <inifrey@gmail.com>
2017-10-14 01:11:33 +0300

    Добавление нового поста админами [Ищу.Киев - Ищу тебя]

--- app/Http/Controllers/FindKievController.php
+++ app/Http/Controllers/FindKievController.php
@@ -14,20 +14,48 @@ class FindKievController extends Controller
     /**
      *  Ищу тебя
      */
+
+    public function __construct()
+    {
+        define('FK_WALL',  'wall-67505812_'); // Ссылка на стену Ищу.Киев
+        define('FK_WALL_API',  '-67505812_');
+    }
+
     public function lookingForYou()
     {
         $user = Auth::user();
-        $lastPost = DB::table('looking_for_you')->orderBy('id', 'desc')->first();
-        $post = Vk::api('wall.getById', ['posts'=>'-'.$lastPost->post_id]);
-        $messages = explode("\n\n", $post['data'][0]->text);
+
+        $date = '2017-10-02';
+
+        $db_posts = DB::table('fk_posts')
+            ->select(DB::raw("post_id"))
+            ->where('post_type', 'lfy')->where('date', $date)
+            ->orderBy('date', 'desc')->get();
+
+        $api_posts_links=""; // Ссылки на записи со стены, для передачи по API
+        foreach ($db_posts as $row) $api_posts_links.= FK_WALL_API.$row->post_id.=',';
+
+        $posts = Vk::api('wall.getById', ['posts'=>$api_posts_links]);
+
+        $messages = array(); // Все сообщения с постов
+
+        foreach ($posts['data'] as $one) {
+            $temp = preg_split("/\\n( |)\\n/", $one->text);
+            array_shift($temp); // Удаляем первый элемент
+            $messages = array_merge($messages,  $temp);
+        }
+
         foreach ($messages as &$message){
             $message = trim($message);
         }
-        array_shift($messages);
 
-        return view('fk.lfy', ['user'=>$user, 'lastPostDate'=>$lastPost->date, 'messages'=>$messages]);
+        return view('fk.lfy', ['user'=>$user, 'lastPostDate'=>$date, 'messages'=>$messages]);
     }
 
+    /**
+     * @param Request $request
+     * @return $this|\Illuminate\Http\RedirectResponse
+     */
     public function lookingForYouAddPost(Request $request){
         if(!Auth::user()->hasRole('administrator')) return back()->withErrors('Доступ запрещен!');
 
@@ -38,14 +66,20 @@ class FindKievController extends Controller
             'link.*' => 'Укажите ссылку на пост!',
         ]);
 
-        if(stristr($request->link, 'vk.com') === FALSE || stristr($request->link, 'wall-67505812') === FALSE) {
+        if(stristr($request->link, 'vk.com') === FALSE || stristr($request->link, FK_WALL) === FALSE) {
             return back()->withErrors('Ссылка должна быть на пост с Ищу.Киев!');
         }
 
-        $linkArr = explode('wall-', $request->link);
+        // TODO:: Проверка, нужный ли пост тут (Ищу тебя)
 
-        DB::table('looking_for_you')->insert(
-            ['post_id' => $linkArr[1], 'date' => Carbon::parse($request->date)->format('Y-m-d')]
+        $linkArr = explode(FK_WALL, $request->link);
+
+        DB::table('fk_posts')->insert(
+            [
+                'post_id' => $linkArr[1],
+                'date' => Carbon::parse($request->date)->format('Y-m-d'),
+                'post_type'=>'lfy'
+            ]
         );
 
         return back();
